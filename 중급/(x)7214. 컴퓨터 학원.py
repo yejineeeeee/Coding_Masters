65233fdb26d55ed335db46072dd77a1769894710
yejin <dpwls0454@naver.com>
2023-08-26 10:40:14 +0900

    효율적인 화폐 구성

---
+++ 중급/(x)7214. 컴퓨터 학원.py
@@ -0,0 +1,19 @@
+def count_placement_cases(N):
+    # dp[i][0]: i열에 학생이 배치되지 않은 경우의 수
+    # dp[i][1]: i열에 학생이 배치된 경우의 수
+    dp = [[0, 0] for _ in range(N + 1)]
+    
+    # 초기
+    dp[1][0] = 1  # 1열에 학생이 배치되지 않은 경우의 수
+    dp[1][1] = 1  # 1열에 학생이 배치된 경우의 수
+    
+    for i in range(2, N + 1):
+        dp[i][0] = dp[i - 1][0] + dp[i - 1][1]  # i열에 학생이 배치되지 않은 경우의 수
+        dp[i][1] = dp[i - 1][0]  # i열에 학생이 배치된 경우의 수
+    
+    total_cases = dp[N][0] + dp[N][1]
+    return total_cases
+
+N = int(input())
+result = count_placement_cases(N)
+print(result)
---
+++ 중급/7213. 태국 택시.py
@@ -0,0 +1,48 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x: # 루트 찾기
+            self.parent[x] = self.find(self.parent[x]) # 부모 바꾸기
+        return self.parent[x] # 루트 반환
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return  # 같은 그룹이면 안함
+
+        if self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x #y의 루트 -> x루트
+        else:
+            self.parent[root_x] = root_y # x의 루트 -> y루트
+            if self.rank[root_x] == self.rank[root_y]:
+                self.rank[root_y] += 1
+
+        return True
+
+def find_minimum_reservation_cost(N, taxi_info):
+    taxi_info.sort(key=lambda x: x[2])  # 택시 예약 비용
+
+    uf = UnionFind(N + 1)
+    min_cost = 0
+    for a, b, c in taxi_info:
+        if uf.union(a, b):
+            min_cost += c # 두 도시 간선을 최소 스패닝 트리에 추가
+    
+    return min_cost
+
+
+N, M = map(int, input().split())
+taxi_info = []
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    taxi_info.append((a, b, c))
+
+# 최소 예약 비용 계산
+result = find_minimum_reservation_cost(N, taxi_info)
+
+print(result)
