1d75daf411c87dc898283fc362c913c60b0572b8
yejin <dpwls0454@naver.com>
2023-08-26 12:23:52 +0900

    누적합 수정

--- 초급/(x)7188. 누적 합.py
+++ 초급/(x)7188. 누적 합.py
@@ -1,36 +1,35 @@
 def build_cumulative_tree(arr):
-    tree = [0] * (len(arr) + 1)
-
-    for i in range(len(arr)):
-        idx = i + 1
-        tree[idx] = arr[i]
-        while idx > 1:
-            idx //= 2
-            tree[idx] += tree[i + 1]
-
+    n = len(arr)
+    tree = [0] * (n * 2)  
+    
+    # 맨 아래에 입력값
+    for i in range(n):
+        tree[n + i] = arr[i]
+    
+    for i in range(n - 1, 0, -1):
+        tree[i] = tree[i * 2] + tree[i * 2 + 1]
+    
     return tree
 
-def print_cumulative_tree(tree, N):
-    power_of_2 = 1
-    idx = 1
+def print_cumulative_tree(tree, n):
+    idx = 1  
+    level = 1  
+    count = 0  
+    
+    for i in range(1, n * 2):
+        if count == level:  # 현재 레벨에서 출력한 원소의 개수가 레벨과 같으면 줄바꿈
+            print()
+            level *= 2
+            count = 0
+        
+        print(tree[i], end=' ')
+        count += 1
+        idx += 1
+        if idx > n:  # 출력할 원소의 개수가 입력된 원소의 개수보다 많으면 종료
+            break
 
-    while idx < len(tree):
-        for _ in range(power_of_2):
-            if idx <= N:
-                print(tree[idx], end=' ')
-                idx += 1
-            else:
-                break
-        print()
-        power_of_2 *= 2
-
-
-N = int(input())
-
-elements = list(map(int, input().split()))
-
+n = int(input())  
+elements = list(map(int, input().split()))  
 
 cumulative_tree = build_cumulative_tree(elements)
-
-
-print_cumulative_tree(cumulative_tree, N)
+print_cumulative_tree(cumulative_tree, n)
