d352880abc277a998feced918c96aabc3cb97611
yejin <dpwls0454@naver.com>
2023-09-01 16:24:48 +0900

    태국 택시

--- 중급/7213. 태국 택시.py
+++ 중급/7213. 태국 택시.py
@@ -1,50 +1,41 @@
-# 크루스칼 알고리즘
+# 크루스칼 알고리즘을 위한 유니온 파인드(Union-Find) 구현
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-class UnionFind:
-    def __init__(self, n):
-        self.parent = [i for i in range(n)]
-        self.rank = [0] * n
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
-    def find(self, x):
-        if self.parent[x] != x: # 루트 찾기
-            self.parent[x] = self.find(self.parent[x]) # 부모 바꾸기
-        return self.parent[x] # 루트 반환
-
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-
-        if root_x == root_y:
-            return  # 같은 그룹이면 안함
-
-        if self.rank[root_x] > self.rank[root_y]:
-            self.parent[root_y] = root_x #y의 루트 -> x루트
-        else:
-            self.parent[root_x] = root_y # x의 루트 -> y루트
-            if self.rank[root_x] == self.rank[root_y]:
-                self.rank[root_y] += 1
-
-        return True
-
-def find_minimum_reservation_cost(N, taxi_info):
-    taxi_info.sort(key=lambda x: x[2])  # 택시 예약 비용
-
-    uf = UnionFind(N + 1)
-    min_cost = 0
-    for a, b, c in taxi_info:
-        if uf.union(a, b):
-            min_cost += c # 두 도시 간선을 최소 스패닝 트리에 추가
-    
-    return min_cost
-
-
+# 입력 받기
 N, M = map(int, input().split())
-taxi_info = []
+
+# 모든 택시 정보를 저장할 리스트 생성
+taxis = []
+
+# 택시 정보 입력 및 정렬
 for _ in range(M):
     a, b, c = map(int, input().split())
-    taxi_info.append((a, b, c))
+    taxis.append((c, a, b))
 
-# 최소 예약 비용 계산
-result = find_minimum_reservation_cost(N, taxi_info)
+taxis.sort()
 
-print(result)
+# 부모 노드 초기화
+parent = [i for i in range(N + 1)]
+
+# 최소 비용 초기화
+total_cost = 0
+
+# 크루스칼 알고리즘 수행
+for cost, a, b in taxis:
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        total_cost += cost
+
+# 출력
+print(total_cost)
