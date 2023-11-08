d08d7e19d68746c8df4720e5fcecf261f2a3eb68
yejin <dpwls0454@naver.com>
2023-11-08 18:58:15 +0900

    좋은 배열

--- 중급/좋은 배열.py
+++ 중급/좋은 배열.py
@@ -4,19 +4,24 @@ def is_good_array(N, a):
 
     for i in range(2 * N):
         if a[i] not in first_occurrence:
+            # 처음 보는 숫자라면, 첫 번째 등장 인덱스를 저장
             first_occurrence[a[i]] = i
         else:
             if a[i] not in second_occurrence:
+                # 이미 한 번 등장한 숫자이지만, 아직 두 번째 등장 인덱스가 없다면, 두 번째 등장 인덱스를 저장
                 second_occurrence[a[i]] = i
             else:
+                # 이미 두 번 이상 등장한 숫자가 또 등장하면 조건을 만족하지 않으므로 "NO" 반환
                 return "NO"
 
     for num, first_index in first_occurrence.items():
         if num in second_occurrence:
             second_index = second_occurrence[num]
+            # 같은 숫자가 두 번 이상 등장한 경우, 두 번째 등장 인덱스와 첫 번째 등장 인덱스의 홀/짝 여부를 비교하여 조건을 확인
             if first_index % 2 == second_index % 2:
                 return "NO"
 
+    # 모든 조건을 통과하면 "YES" 반환
     return "YES"
 
 N = int(input())
